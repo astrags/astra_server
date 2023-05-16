e07d3c3fc0ad64015e446c7f09d0a1b1995c4e38
astrags <astra.globalsat@gmail.com>
2023-05-16 19:46:12 +0200

    gfdgd

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 set -e
 echo "starting astra"
 astra init
-astra -p 8000 -c /etc/astra/astra.conf --daemon
+/bin/astra -p 8000 -c /etc/astra/astra.conf --log /dev/stder --daemon
 echo "astra started"
