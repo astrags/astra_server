218d725fa97d66132cbcef0f235cbe081dc2e04c
astrags <astra.globalsat@gmail.com>
2023-05-16 20:33:36 +0200

    asdfasda

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
-set -e
+
 echo "starting astra"
 astra init
-command="astra -c /etc/astra/astra.conf -p 8000 --log /var/log/astra.log --daemon"
+command="astra -c /etc/astra/astra.conf -p 8000 --log /var/log/astra/astra.log --daemon"
 echo $command
 $($command)
 echo "astra started"
